caee51a8d3a33c163526ff02d26640f05e8afe4f
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-11-01 13:27:55 -0300

    Using a Custom Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Admin, Resource } from 'react-admin'
 import PostIcon from '@mui/icons-material/Book'
 import UserIcon from '@mui/icons-material/Group'
 
+import { Dashboard } from '@/features/dashboard'
 import { PostCreate, PostEdit, PostList } from '@/features/posts'
 import { UserList, UserShow } from '@/features/users'
 
 import { dataProvider } from '@/dataProvider'
 
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource
       name="posts"
       create={PostCreate}
---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { Card, CardContent, CardHeader } from '@mui/material'
+
+export default function Dashboard(props) {
+  return (
+    <Card>
+      <CardHeader title="Welcome to the administration" />
+      <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    </Card>
+  )
+}
