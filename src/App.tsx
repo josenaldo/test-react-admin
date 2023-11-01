2955c91d1c8c76808d9edef664306f8e99200486
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-11-01 13:23:31 -0300

    Adding Search And Filters To The List and Customizing the Menu Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Admin, Resource } from 'react-admin'
 
+import PostIcon from '@mui/icons-material/Book'
+import UserIcon from '@mui/icons-material/Group'
+
 import { PostCreate, PostEdit, PostList } from '@/features/posts'
 import { UserList, UserShow } from '@/features/users'
 
@@ -12,12 +15,14 @@ export const App = () => (
       create={PostCreate}
       edit={PostEdit}
       list={PostList}
+      icon={PostIcon}
     />
     <Resource
       name="users"
       list={UserList}
       show={UserShow}
       recordRepresentation="name"
+      icon={UserIcon}
     />
   </Admin>
 )
--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -3,12 +3,19 @@ import {
   EditButton,
   List,
   ReferenceField,
+  ReferenceInput,
   TextField,
+  TextInput,
 } from 'react-admin'
+
+const postFilters = [
+  <TextInput key="q" source="q" label="Search" alwaysOn />,
+  <ReferenceInput key="user" source="userId" reference="users" label="User" />,
+]
 
 export default function PostList(props) {
   return (
-    <List>
+    <List filters={postFilters}>
       <Datagrid rowClick="edit">
         <TextField source="id" sx={{ fontWeight: 'bold' }} />
         <ReferenceField source="userId" reference="users" link="show" />
